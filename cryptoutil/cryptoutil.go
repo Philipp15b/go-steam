75ea9f0a2cb23072af0a4ff66a28178058509185
Philipp Schroer <hebipp1@googlemail.com>
2014-08-03 12:15:32 +0200

    Minor cryptoutil improvements

--- cryptoutil/cryptoutil.go
+++ cryptoutil/cryptoutil.go
@@ -20,23 +20,19 @@ func SymmetricEncrypt(ciph cipher.Block, src []byte) []byte {
 
 	// pad it, copy the IV to the first 16 bytes and encrypt the rest with CBC
 	encrypted := padPKCS7WithIV(src)
-	for i := 0; i < aes.BlockSize; i++ {
-		encrypted[i] = encryptedIv[i]
-	}
+	copy(encrypted, encryptedIv)
 	cipher.NewCBCEncrypter(ciph, iv).CryptBlocks(encrypted[aes.BlockSize:], encrypted[aes.BlockSize:])
 	return encrypted
 }
 
 // Decrypts data from the reader using AES/CBC/PKCS7 with an IV
-// prepended using AES/ECB/None. It modifies the src slice, but only
-// the returned slice contains the decrypted data.
+// prepended using AES/ECB/None. The src slice may not be used anymore.
 func SymmetricDecrypt(ciph cipher.Block, src []byte) []byte {
-	// get the encrypted IV and decrypt it
 	iv := src[:aes.BlockSize]
 	newECBDecrypter(ciph).CryptBlocks(iv, iv)
-	// decrypt the data
+
 	data := src[aes.BlockSize:]
 	cipher.NewCBCDecrypter(ciph, iv).CryptBlocks(data, data)
-	// remove the padding at the end
+
 	return unpadPKCS7(data)
 }
