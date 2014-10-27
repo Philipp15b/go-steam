61a29b30209b4b571f0b6af8627328430c5460a2
Philipp Schröer <hebipp1@googlemail.com>
2014-10-27 22:18:31 +0100

    Revert commit e13fc01bb4805

--- web.go
+++ web.go
@@ -73,7 +73,7 @@ func (w *Web) apiLogOn() error {
 
 	cryptedSessionKey := cryptoutil.RSAEncrypt(GetPublicKey(EUniverse_Public), sessionKey)
 	ciph, _ := aes.NewCipher(sessionKey)
-	cryptedLoginKey := cryptoutil.SymmetricEncrypt(ciph, []byte(w.SessionId))
+	cryptedLoginKey := cryptoutil.SymmetricEncrypt(ciph, []byte(w.webLoginKey))
 	data := make(url.Values)
 	data.Add("format", "json")
 	data.Add("steamid", strconv.FormatUint(uint64(w.client.SteamId()), 10))
