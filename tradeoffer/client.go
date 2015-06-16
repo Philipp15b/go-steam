743ff513398457e081e2a6776ffb9f49dfb1c756
TwinkerMan <prosto.hoster@gmail.com>
2015-06-17 00:22:22 +0300

    Update client.go

--- tradeoffer/client.go
+++ tradeoffer/client.go
@@ -81,23 +81,22 @@ func (c *Client) Cancel(id TradeOfferId) error {
 }
 
 func (c *Client) Accept(id TradeOfferId) error {
-	baseurl := fmt.Sprintf("https://steamcommunity.com/tradeoffer/%d/", id)
-	req := netutil.NewPostForm(baseurl+"accept", netutil.ToUrlValues(map[string]string{
-		"sessionid":    c.sessionId,
-		"serverid":     "1",
-		"tradeofferid": strconv.FormatUint(uint64(id), 10),
-	}))
-	req.Header.Add("Referer", baseurl)
+    req := netutil.NewPostForm(fmt.Sprintf("https://steamcommunity.com/tradeoffer/%d/accept", id), netutil.ToUrlValues(map[string]string{
+        "sessionid":    c.sessionId,
+        "serverid":     "1",
+    }))
+    req.Header.Add("Referer", fmt.Sprintf("https://steamcommunity.com/tradeoffer/%d/", id))
 
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("accept error: status code %d", resp.StatusCode)
-	}
-	return nil
+    resp, err := c.client.Do(req)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != 200 {
+        return errors.New("accept error: status code not 200")
+    }
+    return nil
 }
 
 type TradeItem struct {
