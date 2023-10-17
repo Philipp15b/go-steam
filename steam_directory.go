d7f180c1d101b19fc201af5516bf3603f2998029
guoguangwu <guoguangwu@magic-shield.com>
2023-10-17 15:27:57 +0800

    chore: unnecessary use of fmt.Sprintf

--- steam_directory.go
+++ steam_directory.go
@@ -31,7 +31,7 @@ func (sd *steamDirectory) Initialize() error {
 	sd.Lock()
 	defer sd.Unlock()
 	client := new(http.Client)
-	resp, err := client.Get(fmt.Sprintf("https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?cellId=0"))
+	resp, err := client.Get("https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?cellId=0")
 	if err != nil {
 		return err
 	}
